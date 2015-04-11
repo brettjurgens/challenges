e970150fab73a802c0f8c1a996683ae4a9e5ac0b
Brett Jurgens <brett@brettjurgens.com>
2015-04-11 12:25:54 -0500

    add code for factors

--- factors_and_caching/factors.rb
+++ factors_and_caching/factors.rb
@@ -1,6 +1,24 @@
 # in:   [10, 5, 2, 20]
 # out:  { 10: [5, 2], 5: [], 2: [], 20: [10, 5, 2]}
 
-def factor list
+def factor(list)
+  list.sort!
+  hash = {}
 
+  list.each_with_index do |number, index|
+    hash[number] = []
+    divisors = list[0, index].clone
+    until divisors.empty?
+      divisor = divisors.pop
+      if number % divisor == 0 && divisor != number
+        hash[number] << divisor
+        hash[number] += hash[divisor] unless hash[divisor].empty?
+        divisors -= hash[divisor]
+      end
+    end
+  end
+
+  hash
 end
+
+puts factor [10, 5, 2, 20]
